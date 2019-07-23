dab69173e6bcd03e0ec242ed2bcaa8644bd70bec
Serge Savel <serge@savel.pro>
2019-07-23 21:13:05 +0400

    Working

--- src/main/java/pro/savel/krp/Controller.java
+++ src/main/java/pro/savel/krp/Controller.java
@@ -16,12 +16,27 @@ public class Controller {
 
 	@PostMapping(path = "/{topic}", consumes = MediaType.APPLICATION_XML_VALUE)
 	@ResponseStatus(HttpStatus.CREATED)
-	public void postAccount(@PathVariable String topic,
-	                        @RequestParam String key,
+	public void postTopic(@PathVariable String topic,
+	                        @RequestParam(required = false) String key,
 	                        @RequestHeader("Content-Length") long contentLength,
 	                        @RequestHeader(name = "Content-Encoding", required = false) String encoding,
-	                        @RequestBody byte[] body) {
+	                        @RequestBody String body) {
 
 		service.postTopic(topic, key, body);
+	}
+
+	@GetMapping(path = "/{topic}")
+	public String getTopic(@PathVariable String topic,
+	                      @RequestParam long offset,
+	                      @RequestParam long limit) {
+
+		return service.getTopicInfo(topic);
+	}
+
+	@GetMapping(path = "/{topic}/{partition}")
+	public String getPartition(@PathVariable String topic, @PathVariable int partition,
+	                       @RequestParam long offset, @RequestParam long limit) {
+
+		return service.getPartition(topic, offset, limit);
 	}
 }
--- src/main/java/pro/savel/krp/Service.java
+++ src/main/java/pro/savel/krp/Service.java
@@ -5,14 +5,49 @@ import org.springframework.kafka.core.KafkaTemplate;
 @org.springframework.stereotype.Service
 public class Service {
 
-	private final KafkaTemplate kafkaTemplate;
+	private final KafkaTemplate<String, String> kafkaTemplate;
 
-	public Service(KafkaTemplate kafkaTemplate) {
+	public Service(KafkaTemplate<String, String> kafkaTemplate) {
 		this.kafkaTemplate = kafkaTemplate;
 	}
 
-	public void postTopic(String topic, String key, byte[] data) {
+	public void postTopic(String topic, String key, String data) {
 
-		kafkaTemplate.send(topic, key, data);
+		if (key == null)
+			kafkaTemplate.send(topic, data);
+		else
+			kafkaTemplate.send(topic, key, data);
+	}
+
+	public String getTopicInfo(String topic) {
+
+		return null;
+	}
+
+	public String getPartition(String topic, long offset, long limit) {
+
+		//props.setProperty("bootstrap.servers", "localhost:9092");
+		//props.setProperty("group.id", "test");
+		//props.setProperty("enable.auto.commit", "false");
+		//props.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		//props.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		//KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+		//consumer.seek();
+		//consumer.subscribe(Arrays.asList("foo", "bar"));
+		//final int minBatchSize = 200;
+		//List<ConsumerRecord<String, String>> buffer = new ArrayList<>();
+		//while (true) {
+		//	ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+		//	for (ConsumerRecord<String, String> record : records) {
+		//		buffer.add(record);
+		//	}
+		//	if (buffer.size() >= minBatchSize) {
+		//		insertIntoDb(buffer);
+		//		consumer.commitSync();
+		//		buffer.clear();
+		//	}
+		//}
+
+		return null;
 	}
 }
