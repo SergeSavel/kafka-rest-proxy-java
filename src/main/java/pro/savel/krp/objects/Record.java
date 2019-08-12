150ef947b5b4e7a2b7ff186aa8dcfda93fbac093
Serge Savel <serge@savel.pro>
2019-08-12 22:10:19 +0400

    Added key UUID.

--- src/main/java/pro/savel/krp/objects/Record.java
+++ src/main/java/pro/savel/krp/objects/Record.java
@@ -1,14 +1,20 @@
 package pro.savel.krp.objects;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Collections;
 import java.util.Map;
+import java.util.UUID;
 
 public class Record {
+
 	public final long timestamp;
 	public final long offset;
 	public final String key;
+	private static ThreadLocal<MessageDigest> threadLocalMD = new ThreadLocal<>();
 	public final Map<String, String> headers;
 	public final String value;
+	public final UUID keyUUID;
 
 	public Record(long timestamp, long offset, String key, Map<String, String> headers, String value) {
 		this.timestamp = timestamp;
@@ -16,5 +22,21 @@ public class Record {
 		this.key = key;
 		this.headers = Collections.unmodifiableMap(headers);
 		this.value = value;
+
+		if (key == null)
+			keyUUID = null;
+		else {
+			MessageDigest md = threadLocalMD.get();
+			if (md == null) {
+				try {
+					threadLocalMD.set(MessageDigest.getInstance("MD5"));
+				} catch (NoSuchAlgorithmException e) {
+					e.printStackTrace();
+				}
+				threadLocalMD.set(md);
+			}
+			keyUUID = UUID.nameUUIDFromBytes(md.digest(key.getBytes()));
+			md.reset();
+		}
 	}
 }
