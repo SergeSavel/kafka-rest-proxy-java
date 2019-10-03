e090d7a5ab9121ade93cb8363c391c3d7233d2d1
Serge Savel <serge@savel.pro>
2019-10-03 19:27:42 +0400

    Encoding bugfix.

--- src/main/java/pro/savel/krp/Service.java
+++ src/main/java/pro/savel/krp/Service.java
@@ -128,7 +128,7 @@ public class Service {
 	private Record createRecord(ConsumerRecord<String, String> consumerRecord) {
 		Map<String, String> headersMap = new HashMap<String, String>();
 		for (Header header : consumerRecord.headers())
-			headersMap.put(header.key(), new String(header.value()));
+			headersMap.put(header.key(), new String(header.value(), StandardCharsets.UTF_8));
 		return new Record(
 			consumerRecord.timestamp(),
 			consumerRecord.offset(),
