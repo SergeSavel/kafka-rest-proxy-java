c03cda43cc3e0fda2b61e544a83e54a1f0565a81
Serge Savel <serge@savel.pro>
2025-03-16 22:56:02 +0400

    Dependencies planning

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,28 @@
 plugins {
-    id("java")
+    id("application")
 }
 
 group = "pro.savel.krp"
-version = "1.0-SNAPSHOT"
+version = "2.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+application {
+    mainClass = "pro.savel.krp.Application"
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("org.springframework:spring-context:6.2.4")
+    implementation("io.netty:netty-all:4.1.119.Final")
+    implementation("org.apache.kafka:kafka-clients:3.9.0")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
