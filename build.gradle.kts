64648519a5399e9c661deb5ef0ab3d448901e262
Serge Savel <serge@savel.pro>
2025-10-03 13:19:28 +0400

    Deps update

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ repositories {
 }
 
 dependencies {
-    implementation("io.netty:netty-all:4.2.4.Final")
+    implementation("io.netty:netty-all:4.2.6.Final")
     implementation("org.apache.kafka:kafka-clients:4.1.0")
     //implementation("org.apache.logging.log4j:log4j:2.24.3")
     implementation("org.slf4j:slf4j-api:2.0.17")
