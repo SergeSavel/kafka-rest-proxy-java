2fede37d5460439cf8ce2bdceb48b34ff72891b0
Serge Savel <serge@savel.pro>
2026-07-28 23:54:43 +0400

    Suppress unchecked warning in UtilsTest for Mockito generic mocks

--- src/test/java/pro/savel/kafka/common/UtilsTest.java
+++ src/test/java/pro/savel/kafka/common/UtilsTest.java
@@ -24,6 +24,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@SuppressWarnings("unchecked")
 class UtilsTest {
 
 //region combineErrorMessage
