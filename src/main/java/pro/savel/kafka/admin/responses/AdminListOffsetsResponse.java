ec365d731934f195e9ef7b77a37462f5a7453053
Serge Savel <serge@savel.pro>
2026-08-03 19:52:51 +0400

    Inline OffsetListing as nested class of AdminListOffsetsResponse

--- src/main/java/pro/savel/kafka/admin/responses/AdminListOffsetsResponse.java
+++ src/main/java/pro/savel/kafka/admin/responses/AdminListOffsetsResponse.java
@@ -14,20 +14,50 @@
 
 package pro.savel.kafka.admin.responses;
 
+import lombok.Getter;
+
 import java.util.ArrayList;
 import java.util.Map;
 
-public class AdminListOffsetsResponse extends ArrayList<OffsetListing> implements AdminResponse {
+public class AdminListOffsetsResponse extends ArrayList<AdminListOffsetsResponse.OffsetListing>
+        implements AdminResponse {
 
     private AdminListOffsetsResponse(int initialCapacity) {
         super(initialCapacity);
     }
 
-    public static AdminListOffsetsResponse of(Map<org.apache.kafka.common.TopicPartition, org.apache.kafka.clients.admin.ListOffsetsResult.ListOffsetsResultInfo> source) {
+    public static AdminListOffsetsResponse of(
+            Map<org.apache.kafka.common.TopicPartition, org.apache.kafka.clients.admin.ListOffsetsResult.ListOffsetsResultInfo> source) {
         if (source == null)
             return null;
         var result = new AdminListOffsetsResponse(source.size());
         source.forEach((topicPartition, info) -> result.add(OffsetListing.of(topicPartition, info)));
         return result;
+    }
+
+    @Getter
+    public static class OffsetListing {
+
+        private String topic;
+        private int partition;
+        private long offset;
+        private long timestamp;
+        private Integer leaderEpoch;
+
+        private OffsetListing() {
+        }
+
+        private static OffsetListing of(org.apache.kafka.common.TopicPartition topicPartition,
+                org.apache.kafka.clients.admin.ListOffsetsResult.ListOffsetsResultInfo info) {
+            if (topicPartition == null || info == null)
+                return null;
+            var result = new OffsetListing();
+            result.topic = topicPartition.topic();
+            result.partition = topicPartition.partition();
+            result.offset = info.offset();
+            result.timestamp = info.timestamp();
+            result.leaderEpoch = info.leaderEpoch().orElse(null);
+            return result;
+        }
     }
 }
